443cd84cb05080455d2fd6e1fb7ec7bf5a19c99d
Ankan5960 <ankanmaity27@gmail.com>
2026-06-12 00:45:49 +0530

    update arrow width and length in origin

--- src/components/ThreeScene.tsx
+++ src/components/ThreeScene.tsx
@@ -74,13 +74,17 @@ const ThreeScene: React.FC<any> = ({
 
     // Inside your scene setup (after creating scene)
     const axisLength = 1;
+    const headLength = 0.2;
+    const headWidth = 0.1;
 
     // X axis → red
     const xArrow = new THREE.ArrowHelper(
       new THREE.Vector3(1, 0, 0), // direction
       new THREE.Vector3(0, 0, 0), // origin
       axisLength,
-      0xff0000 // color
+      0xff0000,// color
+      headLength,
+      headWidth
     );
     scene.add(xArrow);
 
@@ -89,7 +93,9 @@ const ThreeScene: React.FC<any> = ({
       new THREE.Vector3(0, 1, 0),
       new THREE.Vector3(0, 0, 0),
       axisLength,
-      0x00ff00
+      0x00ff00,
+      headLength,
+      headWidth
     );
     scene.add(yArrow);
 
@@ -98,7 +104,9 @@ const ThreeScene: React.FC<any> = ({
       new THREE.Vector3(0, 0, 1),
       new THREE.Vector3(0, 0, 0),
       axisLength,
-      0x0000ff
+      0x0000ff,
+      headLength,
+      headWidth
     );
     scene.add(zArrow);
 
