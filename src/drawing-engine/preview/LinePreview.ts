1f9f989d60a4d087b41cf6c6687cc1075cce1ad4
Ankan5960 <ankanmaity27@gmail.com>
2026-07-04 21:50:54 +0530

    add preview files

---
+++ src/drawing-engine/preview/LinePreview.ts
@@ -0,0 +1,27 @@
+import * as THREE from "three";
+import { PreviewObject } from "./PreviewObject";
+
+export class LinePreview extends PreviewObject<THREE.Line> {
+  protected createObject(): THREE.Line {
+    const geometry = new THREE.BufferGeometry();
+
+    const material = new THREE.LineDashedMaterial({
+      color: 0xffffff,
+      dashSize: 0.2,
+      gapSize: 0.1,
+    });
+
+    return new THREE.Line(geometry, material);
+  }
+
+  update(geometry: THREE.BufferGeometry): void {
+    this.show();
+
+    if (!this.object) return;
+
+    this.object.geometry.dispose();
+    this.object.geometry = geometry;
+
+    this.object.computeLineDistances();
+  }
+}
